f163ff4bc8575f1a91990e19101a394bb2b3ea08
AlfonsoG-dev <alfgomachi@gmail.com>
2024-05-23 15:42:00 -0500

    feat: implements create table query generation

--- src/ORM/Builders/Migration/MigrationBuilder.java
+++ src/ORM/Builders/Migration/MigrationBuilder.java
@@ -1,18 +1,55 @@
 package ORM.Builders.Migration;
 
+import java.sql.Connection;
+
+import ORM.Utils.Formats.UsableMethods;
 import ORM.Utils.Model.ModelUtils;
+import ORM.Utils.Query.MigrationUtils;
 
 public class MigrationBuilder {
     private String tableName;
     private ModelUtils modelUtils;
-    public MigrationBuilder(String tableName) {
+    private MigrationUtils migrationUtils;
+    public MigrationBuilder(String tableName, Connection cursor) {
         this.tableName = tableName;
         modelUtils = new ModelUtils();
+        migrationUtils = new MigrationUtils(cursor);
     }
-
-    public String getCreateTableQuery() {
-        String b = "";
-        return b;
+    public String getCreateDatabaseQuery(String database) {
+        return "CREATE DATABASE IF NOT EXISTS " + database;
+    }
+    public String getCreateTableQuery(UsableMethods m, String type) {
+        String 
+            foreign = "",
+            init = m.initModel();
+        String[]
+            c = modelUtils.getColumns(init, true).split(","),
+            t = modelUtils.getTypes(init, true).split(",");
+        for(String ts: t) {
+            if(ts.contains(".")) {
+                foreign = ts.replace(".", ",");
+            }
+        }
+        String value = "(";
+        for(int i=0; i<c.length; ++i) {
+            t[i] = t[i].replace("'", "");
+            if(t[i].contains(".")) {
+                t[i] = foreign;
+            }
+            value += c[i] + " " + t[i] + ", ";
+        }
+        String
+            v = value.substring(0, value.length()-2),
+            sql = "";
+        switch(type) {
+            case "n":
+                sql = "CREATE TABLE IF NOT EXISTS " + tableName + v + ")";
+                break;
+            case "t":
+                sql = "CREATE TEMPORARY TABLE t_" + tableName + v + ")";
+                break;
+        }
+        return sql;
     }
     public String getCreateIndexQuery() {
         String b = "";
