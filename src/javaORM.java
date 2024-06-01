43b635830494e1f491db459371c66b25c2860afd
AlfonsoG-dev <alfgomachi@gmail.com>
2024-06-01 01:53:10 -0500

    chore: clean from test code

--- src/javaORM.java
+++ src/javaORM.java
@@ -1,20 +1,13 @@
 import java.sql.Connection;
 
 import ORM.DbConnection.Connector;
-import ORM.DbConnection.DAO.QueryDAO;
 
 import ORM.Utils.Formats.DbConfig;
 
-import Samples.Models.Primary.UserModel;
 
 
 public class javaORM {
     public static void main(String[] args) {
-        Connection cursor = initDB();
-        QueryDAO<UserModel> dao = new QueryDAO<>(cursor, "users", new UserModel());
-        for(UserModel m: dao.readAll()) {
-            System.out.println(m.getInstanceData());
-        }
     }
 
     public static Connection initDB() {
