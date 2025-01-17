51ab572732850247f170a4f48bf437c9b0ff7259
AlfonsoG-dev <alfgomachi@gmail.com>
2025-01-16 22:29:51 -0500

    build: change dependencies for build script

--- java-exe.ps1
+++
@@ -1,6 +0,0 @@
-$srcClases = ".\src\ORM\Utils\Query\*.java .\src\ORM\Builders\Migration\*.java .\src\ORM\Utils\Model\*.java .\src\ORM\DbConnection\DAO\*.java .\src\ORM\Utils\Formats\*.java .\src\ORM\DbConnection\Execution\*.java .\src\ORM\Builders\Query\*.java .\src\ORM\DbConnection\*.java .\src\Samples\Migration\*.java .\src\Samples\Query\*.java .\src\Samples\Models\Foreign\*.java .\src\Samples\Models\Primary\*.java "
-$libFiles = ".\lib\mysql-connector-j-8.3.0\mysql-connector-j-8.3.0.jar;"
-$compile = "javac -Werror -Xlint:all -d .\bin\ -cp '$libFiles' $srcClases"
-$createJar = "jar -cfm javaORM.jar Manifesto.txt -C .\bin\ ."
-$runCommand = "$compile" + " && " + "$createJar" 
-Invoke-Expression $runCommand 
