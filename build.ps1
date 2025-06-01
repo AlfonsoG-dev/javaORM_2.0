8168f1aae1bc601066362dd90020c4c1658a2c51
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-01 15:15:10 -0500

    build: update build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$srcClases = ".\src\ORM\Utils\Query\*.java .\src\Samples\Models\Foreign\*.java .\src\ORM\Utils\Formats\*.java .\src\ORM\DbConnection\Execution\*.java .\src\ORM\DbConnection\*.java .\src\Samples\Query\*.java .\src\ORM\Utils\Model\*.java .\src\Samples\Models\Primary\*.java .\src\ORM\DbConnection\DAO\*.java .\src\ORM\Builders\Migration\*.java .\src\ORM\Builders\Query\*.java "
+$srcClases = ".\src\ORM\Utils\Query\*.java .\src\ORM\DbConnection\Execution\*.java .\src\ORM\Utils\Formats\*.java .\src\Samples\Models\Foreign\*.java .\src\Samples\Migration\*.java .\src\ORM\DbConnection\*.java .\src\ORM\Builders\Migration\*.java .\src\ORM\Builders\Query\*.java .\src\Samples\Query\*.java .\src\ORM\Utils\Model\*.java .\src\Samples\Models\Primary\*.java .\src\ORM\DbConnection\DAO\*.java "
 $libFiles = ".\lib\javaORM_2.0\javaORM_2.0.jar;"
 $compile = "javac -Werror -Xlint:all -d .\bin\ -cp '$libFiles' $srcClases"
 $createJar = "jar -cfm javaORM_2.0.jar Manifesto.txt -C .\bin\ ."
